c682efa29f43c0cd322a57c0ea0dfb0e788a4f2f
acolic <aldin.colic@dccsintra.net>
2020-01-29 14:00:28 +0100

    DEV: Aldins first commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./views/app/App.jsx";
 
+// FIXME: Should we have copyrights at the top of the every file?
+
+// FIXME: check package.json file...What should be inside dev-dependencies and what not...
+//  check the ordering in this file...
+
 ReactDOM.render(<App />, document.getElementById("root"));
